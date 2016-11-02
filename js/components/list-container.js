3e9c9eaecf5ed7e2e9b7cae13a76d8196c9e7491
Dan Riding <doonyby@gmail.com>
2016-11-02 16:59:39 -0600

    submit working first time, not second

--- js/components/list-container.js
+++ js/components/list-container.js
@@ -7,27 +7,30 @@ var List = require('./list');
 var ListContainer = React.createClass({
 	getInitialState: function() {
 		return {
-			inputText: 'This is state text',
-			cardsContained: [1,2,3]
+			inputText: '',
+			cardsContained: []
 		}	
 	},
 	onAddInputChange: function() {
-		console.log(this.state.inputText);
-		console.log('changing');
-		// this.setState({
-		// 	inputText: document.getElementById('inputText').value;
-		// });
-
+		this.setState({
+			inputText: document.getElementById('inputText').value
+		});
 	},
 	onAddSubmit: function(event) {
 		event.preventDefault();
-		console.log('form is submitting');
+		console.log(this.state.cardsContained);
+		console.log(this.state.inputText);
+		this.setState({
+			cardsContained: this.state.cardsContained.push(this.state.inputText)
+		});
+		document.getElementById('inputText').value = '';
+		console.log(this.state);
 	},
 	render: function() {
 		return (
-			<div className="list-container">
+			<div className="list-container" onChange={this.onAddInputChange} onSubmit={this.onAddSubmit}>
 				<div className="list-container-title">{this.props.title}</div>
-				<List onChange={this.onAddInputChange} onSubmit={this.onAddsubmit}/>
+				<List />
 			</div>
 		);
 	}
--- js/components/list.js
+++ js/components/list.js
@@ -3,13 +3,13 @@ var ReactDom = require('react-dom');
 
 var Card = require('./card');
 
-var List = function(props) {
+var List = function(props) {	
 	return (
 		<div className="card-list">
 			<Card text="This is card 1" />
 			<Card text="This is card 2" />
 			<Card text="This is card 3" />
-			<form className="listForm" onSubmit={props.onAddSubmit}>
+			<form className="listForm" onSubmit={props.addSubmit}>
 				<input className="textField" type="text" id="inputText" onChange={props.onAddInputChange} name="inputText" />
 				<input className="formBtn" type="submit" value="Submit" />
 			</form>
